0de407d41868829c60222ed2a775ff57a042bc49
eleueu <leu.elv@gmail.com>
2026-08-24 00:48:40 +0300

    Add files via upload
    js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+(function () {
+  var PAGE_W = 393;
+  var PAGE_H = 4674;
+  var page = document.getElementById("page");
+  var stage = document.getElementById("stage");
+  var canZoom = typeof page.style.zoom === "string";
+
+  function fit() {
+    var scale = window.innerWidth / PAGE_W;
+    if (canZoom) {
+      page.style.zoom = String(scale);
+      page.style.transform = "";
+    } else {
+      page.style.transform = "scale(" + scale + ")";
+    }
+    stage.style.height = PAGE_H * scale + "px";
+  }
+
+  fit();
+  window.addEventListener("resize", fit);
+})();
